58be998fa01bb963baa34c8797068c4a90f7be3a
Rez G <mrghaeli@yahoo.com>
2021-04-20 08:17:24 -0700

    first commit

---
+++ src/main/java/com/finalexam/service/FBService.java
@@ -0,0 +1,17 @@
+package com.finalexam.service;
+
+public class FBService {
+
+    /**
+     * If number is divisible by 3, return "Fizz". If divisible by 5,
+     * return "Buzz", and if divisible by both, return "FizzBuzz". Otherwise,
+     * return the number itself.
+     *
+     * Throw IllegalArgumentException for values < 1
+     */
+    public String fBuzz(int number) {
+
+        return "";
+    }
+
+}
